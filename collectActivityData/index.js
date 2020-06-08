ff5ca2cf2c63768211d8bbdb86afb6ccd4ce85e5
Nathan Gaskill <nathang@ANSGRP-002830.local>
2020-06-08 11:31:42 +0100

    Updated function export only teams data

--- collectActivityData/index.js
+++ collectActivityData/index.js
@@ -14,21 +14,41 @@ const main = async function (context, timer) {
 
   // Export AD Users
   const usersData = await msgraph.getAllUsers()
+  context.log(`Total Users: ${usersData.length}`)
+
   const groupedUserData = groupBy(usersData, 'userPrincipalName')
 
   // Get Teams Activity Report Location
   const teamsData = await msgraph.getTeamsUserActivityUserDetail()
 
-  const augmentedTeamsData = teamsData.map(reportItem => Object.assign(reportItem, (groupedUserData[reportItem.userPrincipalName] && groupedUserData[reportItem.userPrincipalName][0]) || {}))
+  const augmentedTeamsData = teamsData
+    // Filter leavers from teams data
+    .filter(reportItem => groupedUserData[reportItem.userPrincipalName])
+    // Enrich Teams Data with user data
+    .map(reportItem => 
+      Object.assign(
+        reportItem,
+        groupedUserData[reportItem.userPrincipalName][0]
+      )
+  );
 
-  // Write user data to Blob
-  const userBlobId = await storage.uploadBlob('user-json', 'user_data.json', JSON.stringify({ value: usersData }))
+  // Active Teams Data
+  console.log(`Active Teams Data: ${augmentedTeamsData.length}`);
+
+
+  // Find the active users without a teams account.
+  const teamsUserArray = augmentedTeamsData.map(
+    ({ userPrincipalName }) => userPrincipalName
+  );
+
+  // Remove the Active users with out teams accounts.
+  const activeTeamsUserData = usersData.filter(user => teamsUserArray.includes(user.userPrincipalName));
+  console.log(`Active Users with Teams account: ${activeTeamsUserData.length}`);
 
   // Write user data to Blob
   const teamsBlobId = await storage.uploadBlob('teams-json', 'teams_activity_data.json', JSON.stringify({ value: augmentedTeamsData }))
 
   // Log Blob Upload Response Id
-  context.log('User JSON Blob Upload Id: ' + userBlobId)
   context.log('Teams JSON Blob Upload Id: ' + teamsBlobId)
 
   // End
